d07a45fd42905c2e1e7063fa71dcb9a0f1d6c8eb
Egemen <egemenc2101@gmail.com>
2024-06-14 23:17:29 +0300

    last

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ const bcryptSalt = bcrypt.genSaltSync(12);
 
 const app = express();
 
-app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(
@@ -111,7 +111,7 @@ app.post('/register', async (req, res) => {
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
   const foundUser = await User.findOne({ username });
-console.log('first', foundUser)
+  console.log('first', foundUser);
   if (foundUser) {
     const passOk = bcrypt.compareSync(password, foundUser.password);
     if (passOk) {
@@ -132,119 +132,121 @@ console.log('first', foundUser)
 app.post('/logout', (req, res) => {
   res.cookie('token', '').json('ok');
 });
-
-const server = app.listen(4000);
-
-const wss = new ws.WebSocketServer({ server });
-wss.on('connection', (connection, req) => {
-  function notifyAboutOnlinePeople() {
-    [...wss.clients].forEach((client) => {
-      client.send(
-        JSON.stringify({
-          online: [...wss.clients].map((c) => ({
-            userId: c.userId,
-            username: c.username,
-          })),
-        })
-      );
-    });
-  }
-  connection.isAlive = true;
-
-  // Send a ping to the client to check if the connection is alive
-  connection.on('ping', () => {
-    connection.isAlive = true;
-    connection.pong();
+if (process.env.API_PORT) {
+  const server = app.listen(process.env.API_PORT, function (err) {
+    if (err) console.log('Error in server setup');
+    console.log('Server listening on Port', process.env.API_PORT);
   });
-
-  // Set up a heartbeat mechanism
-  const heartbeat = setInterval(() => {
-    if (connection.isAlive === false) {
-      clearInterval(heartbeat);
-      connection.terminate(); // Terminate the connection
-      notifyAboutOnlinePeople();
-      console.log('Connection terminated (dead)');
-      return;
-    }
-
-    connection.isAlive = false;
-    connection.ping(); // Send a ping to the client
-  }, 5000);
-
-  // Handle 'pong' messages from the client
-  connection.on('pong', () => {
-    connection.isAlive = true;
-  });
-
-  // Handle connection close event
-  connection.on('close', () => {
-    clearInterval(heartbeat); // Clean up the heartbeat interval
-    notifyAboutOnlinePeople();
-    console.log('Connection closed');
-  });
-
-
-  //read username and id from the cookie for this connection
-  const cookies = req.headers.cookie;
-
-  if (cookies) {
-    const tokenCookieString = cookies
-      .split(';')
-      .find((str) => str.startsWith('token='));
-
-    if (tokenCookieString) {
-      const token = tokenCookieString.split('=')[1];
-      jwt.verify(token, jwtSecret, {}, (err, userData) => {
-        if (err) throw err;
-        const { userId, username } = userData;
-        connection.userId = userId;
-        connection.username = username;
+  const wss = new ws.WebSocketServer({ server });
+  wss.on('connection', (connection, req) => {
+    function notifyAboutOnlinePeople() {
+      [...wss.clients].forEach((client) => {
+        client.send(
+          JSON.stringify({
+            online: [...wss.clients].map((c) => ({
+              userId: c.userId,
+              username: c.username,
+            })),
+          })
+        );
       });
     }
-    connection.on('message', async (message) => {
-      const messageData = JSON.parse(message.toString());
-      const { recipient, text, file } = messageData;
-      let filename = null;
+    connection.isAlive = true;
 
-      if (file) {
-        const parts = file.name.split('.');
-        const ext = parts[parts.length - 1];
-        filename = Date.now() + '.' + ext;
-        const path = __dirname + '/uploads/' + filename
-        const bufferData = Buffer.from(file.data.split(',')[1], 'base64')
-        fs.writeFile(path, bufferData, () => {
-          console.log('file saved:' + path)
-        })
-      }
-
-      if (recipient && (text || file)) {
-        const messageDoc = await Message.create({
-          sender: connection.userId,
-          recipient,
-          text,
-          file: file ? filename : null,
-        });
-        console.log(
-          'created file'
-        );
-        [...wss.clients]
-          .filter((client) => client.userId === recipient)
-          .forEach((client) =>
-            client.send(
-              JSON.stringify({
-                text,
-                sender: connection.userId,
-                recipient,
-                file: file ? filename : null,
-                id: messageDoc._id,
-              })
-            )
-          );
-      }
-      console.log();
+    // Send a ping to the client to check if the connection is alive
+    connection.on('ping', () => {
+      connection.isAlive = true;
+      connection.pong();
     });
-  }
 
-  //notify everyone who connected online (when someone connects)
-  notifyAboutOnlinePeople();
-});
+    // Set up a heartbeat mechanism
+    const heartbeat = setInterval(() => {
+      if (connection.isAlive === false) {
+        clearInterval(heartbeat);
+        connection.terminate(); // Terminate the connection
+        notifyAboutOnlinePeople();
+        console.log('Connection terminated (dead)');
+        return;
+      }
+
+      connection.isAlive = false;
+      connection.ping(); // Send a ping to the client
+    }, 5000);
+
+    // Handle 'pong' messages from the client
+    connection.on('pong', () => {
+      connection.isAlive = true;
+    });
+
+    // Handle connection close event
+    connection.on('close', () => {
+      clearInterval(heartbeat); // Clean up the heartbeat interval
+      notifyAboutOnlinePeople();
+      console.log('Connection closed');
+    });
+
+    //read username and id from the cookie for this connection
+    const cookies = req.headers.cookie;
+
+    if (cookies) {
+      const tokenCookieString = cookies
+        .split(';')
+        .find((str) => str.startsWith('token='));
+
+      if (tokenCookieString) {
+        const token = tokenCookieString.split('=')[1];
+        jwt.verify(token, jwtSecret, {}, (err, userData) => {
+          if (err) throw err;
+          const { userId, username } = userData;
+          connection.userId = userId;
+          connection.username = username;
+        });
+      }
+      connection.on('message', async (message) => {
+        const messageData = JSON.parse(message.toString());
+        const { recipient, text, file } = messageData;
+        let filename = null;
+
+        if (file) {
+          const parts = file.name.split('.');
+          const ext = parts[parts.length - 1];
+          filename = Date.now() + '.' + ext;
+          const path = __dirname + '/uploads/' + filename;
+          const bufferData = Buffer.from(file.data.split(',')[1], 'base64');
+          fs.writeFile(path, bufferData, () => {
+            console.log('file saved:' + path);
+          });
+        }
+
+        if (recipient && (text || file)) {
+          const messageDoc = await Message.create({
+            sender: connection.userId,
+            recipient,
+            text,
+            file: file ? filename : null,
+          });
+          console.log('created file');
+          [...wss.clients]
+            .filter((client) => client.userId === recipient)
+            .forEach((client) =>
+              client.send(
+                JSON.stringify({
+                  text,
+                  sender: connection.userId,
+                  recipient,
+                  file: file ? filename : null,
+                  id: messageDoc._id,
+                })
+              )
+            );
+        }
+        console.log();
+      });
+    }
+
+    //notify everyone who connected online (when someone connects)
+    notifyAboutOnlinePeople();
+  });
+}
+
+module.exports = app;
